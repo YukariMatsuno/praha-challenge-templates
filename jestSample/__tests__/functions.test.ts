095c0be9d18faf9ce4f7406fb3aac670377d7764
yukari matsuno <yukari.matsuno@agaroot.co.jp>
2022-08-25 17:51:10 +0900

    sumOfArrayとasyncSumOfArrayの単体テストを書く

--- jestSample/__tests__/functions.test.ts
+++ jestSample/__tests__/functions.test.ts
@@ -4,3 +4,24 @@ import * as functions from "../functions";
 test('sumOfArray', () => {
     expect(functions.sumOfArray([1,2,3,4])).toBe(10);
 });
+
+test('sumOfArray', () => {
+    expect(functions.sumOfArray([1])).toBe(1);
+});
+
+test("sumOfArray([]) causes exception", (): void => {
+    expect((): number => functions.sumOfArray([])).toThrow();
+  });
+  
+
+test('asyncSumOfArray', () => {
+  return functions.asyncSumOfArray([10, 20]).then((data: number) => {
+    expect(data).toBe(30);
+  })
+});
+
+test('async adds 1 + 2 to equal 3', () => {
+  return functions.asyncSumOfArray([1, 2]).then((data: any) => {
+    expect(data).toBe(3);
+  })
+});
