7b3972a8cd2d5297e5986025173f3a280c909b47
yukari matsuno <yukari.matsuno@agaroot.co.jp>
2022-09-14 18:39:59 +0900

    asyncSumOfArraySometimesZeroのテストを追加

--- jestSample/__tests__/functions.test.ts
+++ jestSample/__tests__/functions.test.ts
@@ -25,3 +25,24 @@ test('async adds 1 + 2 to equal 3', () => {
     expect(data).toBe(3);
   })
 });
+
+
+jest.mock("../util");
+
+describe("asyncSumOfArraySometimesZero", (): void => {
+  test("empty array returns 0", (): Promise<void> => {
+    return expect(functions.asyncSumOfArraySometimesZero([])).resolves.toBe(0);
+  });
+
+  test.each([
+    { expected: -7, input: [-7] },
+    { expected: +3, input: [1, 2] },
+    { expected: -4, input: [0, 5, -9, 0] },
+  ])(
+    "%o",
+    ({ expected, input }): Promise<void> => {
+      // eslint-disable-next-line prettier/prettier
+      return expect(functions.asyncSumOfArraySometimesZero(input)).resolves.toBe(expected);
+    }
+  );
+});
